0bc20a8d57bd3754d4ed9d0b1f6d3ceb5fc0fca5
Daniel Kriesten <krid@tu-chemnitz.eu>
2015-06-30 16:02:58 +0200

    [update] now we get the result in JSON

--- iCalOverview.widget/ical_overview.swift
+++ iCalOverview.widget/ical_overview.swift
@@ -24,34 +24,157 @@ println()
 //Legt ein Array mit allen Kalendern vom Typ "Event" an , Typ Kalender gefordert...
 var calendars  = store.calendarsForEntityType(EKEntityTypeEvent) as [EKCalendar]
 
-//...damit muss der Datentyp für calendar hier nicht spezifiziert werden...
-for calendar in calendars
+//Gregorianischer Kalender
+let nsCalendar = NSCalendar.currentCalendar()
+
+var dateFormatterDatum : NSDateFormatter = NSDateFormatter()
+dateFormatterDatum.dateFormat = "dd.MM.yyyy"
+
+var dateFormatterUhrzeit : NSDateFormatter = NSDateFormatter()
+dateFormatterUhrzeit.dateFormat = "HH:mm"
+
+var calendar : EKCalendar
+
+
+var firstDay = true
+
+
+/*
+
+JSON - Syntax:
+
+EndergebnisArray:           [datumObject1, datumObject2,...]
+datumobject:                {datum : [calendarObject1, calendarObject2,...]}
+calendarObject:             {Kalendertitel : [eventObject1, eventObject2,...]}
+eventObject:                {start:Uhrzeit, ende: Uhrzeit, titel: Eventtitel}
+
+*/
+
+
+
+
+//Macht aus einem EKEvent ein eventObject mit titel, startzeit und endzeit
+
+func eventObjectForEvent(event: EKEvent) -> String
 {
-    //...sondern es kann direkt auf die member von EKCalendar zugegriffen werden.
-    println("\(calendar.title!):")
-
-    //vor 3 Tagen
-    var startDate : NSDate = NSDate().dateByAddingTimeInterval(-60*60*24*3)
+    var eventString = "{"
+    //println("    titel: \(event.title)")
+    eventString += "\"titel\": \"\(event.title)\","
     
-    //in 3 Tagen
-    var endDate : NSDate = NSDate().dateByAddingTimeInterval(60*60*24*3)
+    //println("    start: \(dateFormatterUhrzeit.stringFromDate(event.startDate))")
+    eventString += "\"start\": \"\(dateFormatterUhrzeit.stringFromDate(event.startDate))\","
     
-    //Legt ein NSPredicate für Events an (hier: Startdatum, Enddatum, zu durchsuchendes Kalenderarray) , NSPredicate bietet viele Möglichkeiten Collections zu durchsuchen: http://nshipster.com/nspredicate/
+    //println("    ende: \(dateFormatterUhrzeit.stringFromDate(event.endDate))")
+    eventString += "\"ende\": \"\(dateFormatterUhrzeit.stringFromDate(event.endDate))\""
+    
+    eventString += "}"
+    
+    return eventString
+}
 
-    var predicate = store.predicateForEventsWithStartDate( startDate,
-        endDate: endDate,
-        calendars: [calendar])
 
-    //analog zu den Kalendern alle events, die dem Prädikat entsprechen
-    var events = store.eventsMatchingPredicate(predicate) as [EKEvent]
-
+//Macht aus einem EKEvent-Array ein JSON Array
+func eventArrayForEvents(events:[EKEvent]) -> String
+{
+    var eventArrayString = "["
+    
+    var firstEvent = true
     for event in events
     {
-        //Ausgabe event.title, startDate, endDate
-        println("   \(event.title) (von \(event.startDate) bis \(event.endDate))")
+        if !firstEvent {eventArrayString += ","}
+        else {firstEvent = false}
+        eventArrayString += eventObjectForEvent(event)
     }
-
+    
+    eventArrayString += "]"
+    
+    return eventArrayString
 }
+
+//Macht aus Startdatum, Enddatum und EKCalendar ein calendarObject (leer falls keine Events vorhanden)
+func calendarObjectForStartDate(startDate: NSDate, endDate: NSDate, calendar: EKCalendar) -> String
+{
+    var calendarObjectString: String
+    
+    let predicate = store.predicateForEventsWithStartDate( startDate,
+        endDate: endDate,
+        calendars: [calendar])
+    
+    let events = store.eventsMatchingPredicate(predicate) as [EKEvent]
+    
+    let eventArrayString = eventArrayForEvents(events)
+    
+    if(eventArrayString == "[]")
+    {
+        calendarObjectString = ""
+    }
+    else
+    {
+        calendarObjectString = "{\"\(calendar.title)\":\(eventArrayString)}"
+    }
+    return calendarObjectString
+}
+
+//Liefert ein JSON Array aus calendarObjects
+
+func calendarArrayForDate(startDate: NSDate, endDate: NSDate,calendars:[EKCalendar]) -> String
+{
+    var calendarArrayString = "["
+    
+    var firstCalendar = true
+    for calendar in calendars
+    {
+        let calendarObject = calendarObjectForStartDate(startDate, endDate, calendar)
+        
+
+            if calendarObject != ""
+            {
+                if !firstCalendar{calendarArrayString += ","}
+                else {firstCalendar = false}
+                calendarArrayString += calendarObject
+            }
+    }
+    
+    calendarArrayString += "]"
+    
+    return calendarArrayString
+}
+
+
+// hier beginnt die Ausführung:
+
+
+for var day: Int = 0; day <= 4; day++
+{
+    
+
+    
+    let date = NSDate().dateByAddingTimeInterval(NSTimeInterval(24*60*60*day))
+    var components = nsCalendar.components( .CalendarUnitYear | .CalendarUnitMonth | .CalendarUnitDay, fromDate: date)
+        
+    let startDate = nsCalendar.dateFromComponents(components)
+        
+    components.day += 1
+    let endDate = nsCalendar.dateFromComponents(components)
+    
+    println(dateFormatterDatum.stringFromDate(date))
+    
+    for calendar in calendars
+    {
+        println(calendarObjectForStartDate(startDate!, endDate!, calendar))
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
